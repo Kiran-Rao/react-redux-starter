ac72a6673365f0ec36ffd4a7f5213d4e38c7c833
Kiran Rao <kiran_rao@outlook.com>
2017-04-06 14:32:14 -0400

    Change truthy to true

--- src/utils/is-promise.test.js
+++ src/utils/is-promise.test.js
@@ -5,7 +5,7 @@ const wrap = (promise) => ({ promise });
 
 test('should return true if Promise provided', t => {
   const promise = new Promise((resolve) => resolve(false));
-  t.truthy(isPromise(wrap(promise)));
+  t.true(isPromise(wrap(promise)));
 });
 
 test('should return false if not a promise', t => {
@@ -13,7 +13,7 @@ test('should return false if not a promise', t => {
   const badPayload2 = ['hello', 'world'];
   const badPayload3 = 'hello world';
 
-  t.falsy(isPromise(wrap(badPayload1)));
-  t.falsy(isPromise(wrap(badPayload2)));
-  t.falsy(isPromise(wrap(badPayload3)));
+  t.false(isPromise(wrap(badPayload1)));
+  t.false(isPromise(wrap(badPayload2)));
+  t.false(isPromise(wrap(badPayload3)));
 });
