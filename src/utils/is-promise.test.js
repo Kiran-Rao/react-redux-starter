47578fa0dca6a5ba0a2650b5958ed2c9195c7969
Kiran Rao <Kiran-Rao@users.noreply.github.com>
2017-04-06 15:12:09 -0400

    Ava (#14)
    * Cleanup unused scripts
    
    * Remove fire-action
    
    * Add ava tests
    
    * Force color in CircleCI
    
    * Convert CLI args to package
    
    * snapshot test
    
    * Add update snapshots script
    
    * Remove karma
    
    * remove enzyme
    
    * Remove coverage
    
    * cleanup
    
    * Ava Test lint
    
    * Change truthy to true
    
    * Change warn to 1 in eslint
    
    * Reorder package.json
    
    * Don't clean before test
    
    * Fix lint

--- src/utils/is-promise.test.js
+++ src/utils/is-promise.test.js
@@ -1,25 +1,19 @@
+import test from 'ava';
 import isPromise from './is-promise';
 
-describe('isPromise', () => {
-  it('should return true if a Promise is provided', () => {
-    const promise = new Promise((resolve) => resolve(true));
+const wrap = (promise) => ({ promise });
 
-    const payload = {
-      promise
-    };
+test('should return true if Promise provided', t => {
+  const promise = new Promise((resolve) => resolve(false));
+  t.true(isPromise(wrap(promise)));
+});
 
-    expect(isPromise(payload)).toBe(true);
-  });
+test('should return false if not a promise', t => {
+  const badPayload1 = { hello: 'world' };
+  const badPayload2 = ['hello', 'world'];
+  const badPayload3 = 'hello world';
 
-  it('should return false if something that is not a Promise is provided', () => {
-    const badPayload1 = { hello: 'world' };
-    const badPayload2 = ['hello', 'world'];
-    const badPayload3 = 'hello world';
-    const badPayload4 = 'hello world';
-
-    expect(isPromise({ promise: badPayload1 })).toBe(false);
-    expect(isPromise({ promise: badPayload2 })).toBe(false);
-    expect(isPromise({ promise: badPayload3 })).toBe(false);
-    expect(isPromise({ promise: badPayload4 })).toBe(false);
-  });
+  t.false(isPromise(wrap(badPayload1)));
+  t.false(isPromise(wrap(badPayload2)));
+  t.false(isPromise(wrap(badPayload3)));
 });
