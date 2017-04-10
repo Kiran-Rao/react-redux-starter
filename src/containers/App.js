4c99255657b23f1ef1356633412ffd73eee3971d
Kiran Rao <kiran_rao@outlook.com>
2017-04-10 18:13:26 -0400

    Add MVC button clicks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-function App(props) {
-  console.log(props);
+import { increment, decrement } from '../actions/counter';
+import Button from '../components/button';
+
+const App = (props) => {
   return (
     <div>
-      Hello world!
+      <div>
+        Hello world! The count is: {props.count}
+      </div>
+      <Button title="Increase" click={props.increment} />
+      <Button title="Decrease" click={props.decrement} />
     </div>
   );
-}
+};
 
-export default App;
+const mapStateToProps = (state) => ({
+  count: state.counter.get('count')
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  increment: () => dispatch(increment()),
+  decrement: () => dispatch(decrement())
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
