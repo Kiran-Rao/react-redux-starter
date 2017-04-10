3a20ec8935849e1dac39127e63d7b67d037e4a5f
Kiran Rao <kiran_rao@outlook.com>
2017-04-10 18:30:56 -0400

    Testing

---
+++ src/components/index.test.js
@@ -0,0 +1,15 @@
+import test from 'ava';
+import React from 'react';
+import render from 'react-test-renderer';
+
+import { Button, Welcome } from './';
+
+test('Button snapshot', t => {
+  const tree = render.create(<Button title="aoeu" click={() => {}} />).toJSON();
+  t.snapshot(tree);
+});
+
+test('Welcome snapshot', t => {
+  const tree = render.create(<Welcome count={5} />).toJSON();
+  t.snapshot(tree);
+});
