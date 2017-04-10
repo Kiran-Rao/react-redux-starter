803362b15fe9598e97f3e970251d5d4dc94c3d87
Kiran Rao <kiran_rao@outlook.com>
2017-04-10 15:18:46 -0400

    change to function

--- src/containers/app.js
+++ src/containers/app.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const App = () => (
-  <div>
-    Hello world!
-  </div>
-);
+function App(props) {
+  console.log(props);
+  return (
+    <div>
+      Hello world!
+    </div>
+  );
+}
 
 export default App;
