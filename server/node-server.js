5ec29750043920225b366109632d944780b8eca4
Kiran Rao <Kiran-Rao@users.noreply.github.com>
2017-04-09 20:46:01 -0400

    Use express instead of http-server (#18)
    * Change dev-server port to 3000
    
    * Replace http-server with express server
    
    * Update server message

---
+++ server/node-server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const http = require('http');
+const winston = require('winston');
+const helmet = require('helmet');
+const nodeAppServer = require('./node-app-server');
+const bodyParser = require('body-parser');
+
+/**
+ * Heroku-friendly production http server.
+ *
+ * Serves your app and allows you to proxy APIs if needed.
+ */
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Enable various security helpers.
+app.use(helmet());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Serve the distributed assets and allow HTML5 mode routing. Must be last.
+nodeAppServer(app);
+
+// Start up the server.
+const server = http.createServer(app);
+server.listen(PORT, (err) => {
+  if (err) {
+    winston.error(err);
+    return;
+  }
+
+  const addr = server.address();
+  winston.info(`Listening on at ${addr.address}:${addr.port}`);
+});
