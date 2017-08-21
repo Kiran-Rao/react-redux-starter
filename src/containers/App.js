d1d2a68d9f4c3ba74d55ba2a2c2226f9ebd11089
Kiran Rao <Kiran-Rao@users.noreply.github.com>
2017-08-20 21:19:13 -0400

    Combine actions, reducers and actions into a single "Duck" (#31)

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import { increment, decrement } from '../actions/counter';
+import { increment, decrement } from '../redux/counter/actions';
 import Welcome from '../components/Welcome';
 import Button from '../components/Button';
 
